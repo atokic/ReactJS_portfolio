1d659a5118467d36a8aee0f4b11c0e6d4109ce52
Ante <atokic15@gmail.com>
2024-06-30 15:27:23 +0200

    Added Vercel Speed Insights

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ function App() {
           <Contact />
           <Footer />
           <FooterBottom />
-          <SpeedInsights />
         </div>
+          <SpeedInsights />
     </div>
   );
 }
