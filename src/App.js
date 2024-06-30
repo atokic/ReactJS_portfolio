27e73e35d3bb33fcc21c7856eb1b8acf701b9b72
Ante <atokic15@gmail.com>
2024-06-30 12:02:11 +0200

    Added Vercel Speed Insights

--- src/App.js
+++ src/App.js
@@ -10,21 +10,24 @@ import Projects from "./components/projects/Projects";
 import Resume from "./components/resume/Resume";
 import Testimonial from "./components/testimonial/Testimonial";
 
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 function App() {
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText px-4">
         <Navbar />
-      <div className="max-w-screen-xl mx-auto">
-        <Banner />
-        <Features />
-        <Hobbies />
-        <Projects />
-        <Resume />
-        {/* <Testimonial /> */}
-        <Contact />
-        <Footer />
-        <FooterBottom />
-      </div>
+        <div className="max-w-screen-xl mx-auto">
+          <Banner />
+          <Features />
+          <Hobbies />
+          <Projects />
+          <Resume />
+          {/* <Testimonial /> */}
+          <Contact />
+          <Footer />
+          <FooterBottom />
+        </div>
+        <SpeedInsights />
     </div>
   );
 }
