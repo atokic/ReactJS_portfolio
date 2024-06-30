1a53cb5e120e55b35dffe09655e1dffdba97df1d
Ante <atokic15@gmail.com>
2024-06-30 15:48:08 +0200

    Added Vercel Analytics

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "slick-carousel/slick/slick.css";
 import './index.css';
 import App from './App';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <SpeedInsights />
+    <Analytics/>
     <App />
   </>
 );
