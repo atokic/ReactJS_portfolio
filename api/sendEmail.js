f392db395033a4d3cccec903e713d0f06baeb8f0
Ante <atokic15@gmail.com>
2024-07-17 12:54:20 +0200

    Fixed bugs

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -22,17 +22,49 @@ export default async function handler(req, res) {
     });
 
     const mailOptions = {
-      from: email,
-      to: process.env.RECIPIENT_EMAIL, // Use environment variables
-      subject: `${subject} - from ${username}`,
-      text: `Phone: ${phoneNumber}\n\n${message}`,
-    };
+        from: email,
+        to: process.env.RECIPIENT_EMAIL,
+        subject: `${subject} - from ${username}`,
+        html: `
+          <html>
+            <head>
+              <style>
+                body {
+                  font-family: Arial, sans-serif;
+                  background-color: #f4f4f4;
+                  padding: 20px;
+                }
+                .email-content {
+                  background-color: #fff;
+                  padding: 20px;
+                  border-radius: 8px;
+                  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+                }
+                .signature {
+                  margin-top: 20px;
+                  font-style: italic;
+                }
+              </style>
+            </head>
+            <body>
+              <div class="email-content">
+                <p><strong>Name:</strong> ${username}</p>
+                <p><strong>Phone:</strong> ${phoneNumber}</p>
+                <p><strong>Subject:</strong> ${subject}</p>
+                <p>${message}</p>
+              </div>
+              <p class="signature">Sent from my personal website atokic.com!</p>
+            </body>
+          </html>
+        `,
+      };
 
     try {
       const info = await transporter.sendMail(mailOptions);
-      res.status(200).json({ message: "Email sent: " + info.response });
+      const successMessage = `Thank you, ${username}! Your message has been sent successfully.`;
+      res.status(200).json({ message: successMessage });
     } catch (error) {
-      res.status(500).json({ error: error.toString() });
+      res.status(500).json({ error: "Failed to send the email. Please try again later." });
     }
   } else {
     res.status(405).json({ message: "Only POST requests are allowed" });
